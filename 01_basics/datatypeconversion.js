91cc30ca96d21bd7e4d1491112a20c1e033fbe74
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-06 07:10:28 +0000

    conversion

--- 01_basics/datatypeconversion.js
+++ 01_basics/datatypeconversion.js
@@ -39,4 +39,29 @@ let stringNumber = String(someNumber)
 
 let value = 3
 let negValue = -value
-console.log(negValue);
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(3-2);
+// console.log(2*2);
+// console.log(4/2);
+// console.log(7%3);
+
+let str1 = "Hello"
+let str2 = " Pradeep"
+ 
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
