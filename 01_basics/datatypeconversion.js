bc3b10cc579c3d8fa4b81005f75af074d9158cc9
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-06 06:32:54 +0000

    conversion

---
+++ 01_basics/datatypeconversion.js
@@ -0,0 +1,36 @@
+let score = "44asbd";
+score = null;
+score = true;
+score = undefined;
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// change datatype 
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "44" => 44
+// "44asdb" => NaN(Not a Number)
+// true => 1; false => 0
+
+let isLoggedIn  = 1
+
+isLoggedIn = "Pradeep"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false;
+// "" => false
+// "Pradeep" => true;
+
+/* Cahnge strng to Number */ 
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
