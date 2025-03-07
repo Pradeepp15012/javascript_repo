d227db11373e7ef75d80d80e9ca772f638cbf1d3
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-07 06:11:48 +0000

    numandmath

---
+++ 01_basics/numsandmath.js
@@ -0,0 +1,43 @@
+const score = 500
+// console.log(score);
+
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.8966
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+/*************Maths Library **********/
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.6));
+
+// console.log(Math.ceil(4.2));
+
+
+// console.log(Math.floor(4.9));
+
+
+// console.log(Math.min(453, 233, 536, 843));
+// console.log(Math.max(423, 343, 664, 447));
+
+console.log(Math.random()); // Math.random ki value hamesha 0 and 1 bich me aayegi
+
+console.log((Math.random()*10) + 1);
+
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10
+const max = 10
+
+console.log(Math.floor(Math.random() + (max - min + 1)) + min);
