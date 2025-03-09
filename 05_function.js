026c625b1092e8c9caa2e17d8e4aaaabe6b8cd71
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-09 08:00:15 +0000

    functions

---
+++ 05_function.js
@@ -0,0 +1,48 @@
+function sayMyName(){
+    console.log("P");
+    console.log("R");
+    console.log("A");
+    console.log("D");
+    console.log("E");
+    console.log("E");
+    console.log("P");
+    
+}
+
+// sayMyName()
+
+/** jab user function ko define karta hai to jo bhi 
+input leta hai use parameter kahte hai and jab user 
+function ko call karate hai tb jo bhi input lete hai 
+use argument kahte hai
+ */
+// function addTwoNumbers(number1, number2){
+//     console.log(number1 + number2)
+// }
+
+// addTwoNumbers(4, "7") // 47
+
+// addTwoNumbers(4, null) // 4
+
+// addTwoNumbers(4, 6)
+
+function addTwoNumbers(number1, number2){
+    // let result = number1 + number2
+    // console.log(result);
+    return number1 + number2
+}
+const result = addTwoNumbers(4, 6)
+
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "sam"){
+    // if(username === undefined){
+    if(!username){
+        console.log("Please enter a username");
+        return
+    }
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage(""));
+
+console.log(loginUserMessage("pradeep"));
