6239a56bbd148b3d89c83dbc404b38ab87e1af90
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-09 06:55:28 +0000

    json

---
+++ 04_objdestructure.js
@@ -0,0 +1,29 @@
+const course = {
+    coursename: "js in hindi",
+    prize: "999",
+    courseInstructor: "Pradeep"
+
+}
+
+/** De- structure of object */
+
+// createImageBitmap.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// API 
+
+// {
+//     "name": "Pradeep",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+[
+    {},
+    {},
+    {}
+]
