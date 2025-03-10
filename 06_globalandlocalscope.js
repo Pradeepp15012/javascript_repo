7ed68703f399d1a95d56298b5087cc6f04964860
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-10 06:48:15 +0000

    scop

---
+++ 06_globalandlocalscope.js
@@ -0,0 +1,60 @@
+// let a = 10
+// const b = 20
+// var c = 30
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// if(true){
+//     let a = 10
+//     const b = 20
+//     var c = 30
+// }
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+
+// function one(){
+//     const username = "Pradeep"
+
+//     function two(){
+//         const website = "youtube"
+//         console.log(username);
+        
+//     }
+//     // console.log(website);
+//     // two()
+// }
+
+// one()
+
+// if(true){
+//     const username = "Pradeep"
+//     if(username === "Pradeep"){
+//         const website = " youtube"
+//         console.log(username + website);
+        
+//     }
+//     // console.log(website);
+    
+// }
+// console.log(username);
+
+
+/** Interesting  **/
+console.log( addone(5));
+
+
+function addone(num) {
+    return num + 1
+}
+
+
+const addTwo = function(num){
+    return num + 2
+}
+addTwo(5)
