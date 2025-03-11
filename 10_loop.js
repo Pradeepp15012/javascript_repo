ddd03c911715f9723f9dcd2721e7dc50104f47df
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-11 17:48:51 +0000

    loop

--- 10_loop.js
+++ 10_loop.js
@@ -17,18 +17,18 @@ for (let i = 0; i <= 10; i++) {
     }    
 }*/
 
-let myArray = ["flash", "iostream", "superman"]
+/*let myArray = ["flash", "iostream", "superman"]
 
 console.log(myArray.length);
 for(let index = 0; index < myArray.length; index++){
     const element = myArray[index];
     console.log(element);
     
-}
+} */
 
 // break and continue
 
-for(let index = 0; index <= 20; index++){
+/* for(let index = 0; index <= 20; index++){
     if(index == 5){
         console.log(`Detected 5`);
         break
@@ -36,4 +36,26 @@ for(let index = 0; index <= 20; index++){
     console.log(`value of i is ${index}`);
     
     const element = Array[index];
+} */
+
+
+let index = 0
+
+while (index <= 10) {
+    console.log(`value of index is ${index}`);
+    index = index + 2
 }
+
+let myArray = ["flash", "iostream", "superman"]
+
+let arr = 0
+while(arr < myArray.length){
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1    
+}
+
+let score = 1
+do{
+    console.log(`Score is ${score}`);
+    score++
+}while(score <= 10);
