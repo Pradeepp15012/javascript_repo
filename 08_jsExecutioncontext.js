7a1fefd12817a3f93fd665da8c520ee64af9c3bc
Pradeep Kumar <122957394+Pradeepp15012@users.noreply.github.com>
2025-03-11 05:44:31 +0000

    basics

---
+++ 08_jsExecutioncontext.js
@@ -0,0 +1,13 @@
+/** Js execution context '
+ Global Execution context : this key
+global execution context
+function execution context
+End Execution context
+
+
+Memory creation phase
+Execution Phase
+
+1- Global Execution -- this
+2- Memory Phse
+*/
